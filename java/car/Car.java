6fd308b6374d3da635f12edcdb626d41e6345d83
cnzau <clement.nzau@gmail.com>
2016-04-07 07:22:26 +0300

    object using constructor class

---
+++ java/car/Car.java
@@ -0,0 +1,17 @@
+package car;
+
+public class Car {
+	int noOfWheels;
+	int noOfDoors;
+	int carSeats;
+	String color;
+	String transmission;
+	
+	public Car(int wheels,	int doors,	int carSeats,	String color,	String transmission) {
+		this.noOfWheels = wheels;
+		this.noOfDoors = doors;
+		this.carSeats = carSeats;
+		this.color = color;
+		this.transmission = transmission;
+	}
+}
---
+++ java/car/main.java
@@ -0,0 +1,11 @@
+package car;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car volvo = new Car(4,5,7,"white","manual");
+		
+	}
+
+}
